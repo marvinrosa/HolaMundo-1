8c2da6db681ef1fa101d26f85622850989fd1fd2
“Marvin” <marvin.fortin@outlook.com>
2017-03-14 21:13:08 -0500

    Mensaje Bienvenida- Falta errores

--- app/src/main/java/com/example/soporte/holamundo/Principal.java
+++ app/src/main/java/com/example/soporte/holamundo/Principal.java
@@ -1,5 +1,6 @@
 package com.example.soporte.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,23 +11,43 @@ import android.widget.TextView;
 public class Principal extends AppCompatActivity {
 
     private EditText cajaNombre;
-    private TextView cajaMensaje;
+    private TextView cajaApellido;
+    private Intent i;
+    private Bundle b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        //Capturamos las cajas
         cajaNombre = (EditText)findViewById(R.id.txtNombre);
-       // cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
+        cajaApellido = (EditText)findViewById(R.id.txtApellido);
+        i =  new Intent(this,Saludo.class);
+        b = new Bundle();
+
     }
 
 
-    public void saludar(View v){
-        String aux;
-       // aux = cajaNombre.getText().toString();
-        //cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
+    public void Saludar(View v){
+       String nomb, apell;
 
+        // Se toma el valor de la persona q ingreso los datos
+        nomb= cajaNombre.getText().toString();
+        apell= cajaApellido.getText().toString();
+
+        b.putString("Nombre",nomb);
+        b.putString("Apellido",apell);
+
+
+        i.putExtras(b);
+
+        startActivity(i);
+
+    }
+
+    public boolean validar(){
+        return  true;
     }
 }
 
--- app/src/main/java/com/example/soporte/holamundo/Saludo.java
+++ app/src/main/java/com/example/soporte/holamundo/Saludo.java
@@ -1,13 +1,33 @@
 package com.example.soporte.holamundo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Saludo extends AppCompatActivity {
+
+    private TextView saludo;
+    private Bundle b;
+    private String aux, nomb, apell;
+    private Resources res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+        saludo = (TextView)findViewById(R.id.txtSaludo);
+
+        b = getIntent().getExtras();
+        nomb = b.getString("Nombre");
+        apell = b.getString("Apellido");
+
+        res = this.getResources();
+
+        aux = res.getString(R.string.parte_saludo)+ " "+ nomb + " " + apell + " " + res.getString(R.string.parte_saludo2);
+
+        saludo.setText(aux);
+
     }
 }
